260562abda6d95ab2b0a7fd025b63d38a3d31fd3
ankithshetty0505 <ankithshetty0505@gmail>
2026-03-25 10:00:06 +0530

    Testing auto-deploy

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello VVCE ISE 6th Sem! DevOps project running."
+    return "VVCE DevOps: Automated Deployment is 100% Successful!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
